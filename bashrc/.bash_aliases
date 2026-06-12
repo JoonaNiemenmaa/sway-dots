7af3960113e56e6969c6848b22dfb4a76d826005
Joona Niemenmaa <joonaoskari@hotmail.com>
2026-06-12 17:30:42 +0300

    EN HUGE COMMIT

--- bashrc/.bash_aliases
+++
@@ -1,2 +0,0 @@
-alias cdfzf="cd \$(find . -type d | fzf)"
-alias python="python3"
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,10 +5,12 @@ vim.g.mapleader = ' '
 -- See `:h vim.o` NOTE: You can change these options as you wish! For more options, you can see `:h option-list`
 -- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'` (Note the single quotes)
 
-vim.opt.shiftwidth = 4         -- changes indentation length
-vim.opt.tabstop = 4            -- changes the length of tab
+vim.opt.shiftwidth = 4         -- changes indentation length vim.opt.tabstop = 4            -- changes the length of tab
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
 vim.opt.fileencoding = "utf-8" -- makes the file encoding utf-8 by default
-vim.opt.relativenumber = true  -- a setting for relative line numbers
+vim.opt.relativenumber = false  -- a setting for relative line numbers
 vim.opt.number = true          -- line numbers
 vim.opt.splitbelow = true      -- place any horizontal splits on the bottom of the screen
 vim.opt.splitright = true      -- same as above but for vertical splits
@@ -21,12 +23,12 @@ vim.opt.virtualedit = "block"  -- Makes empty cells selectable in VISUAL BLOCK m
 vim.opt.ignorecase = false     -- Pretty self explanatory
 vim.opt.title = true           -- Display extra information in window title
 vim.opt.termguicolors = true   -- Increases the color range available for the terminal
-vim.opt.wrapscan = false       -- searches wrap around the file
-vim.o.list = false             -- Show <tab> and trailing spaces.
+vim.opt.wrapscan = true        -- searches wrap around the file
+vim.opt.list = false           -- Show <tab> and trailing spaces.
 
 -- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
-vim.o.confirm = true
+-- vim.o.confirm = true
 
 
 -- KEYMAPS
@@ -61,19 +63,19 @@ vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
 -- Highlight when yanking (copying) text.
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	callback = function()
-		vim.hl.on_yank()
-	end,
+    desc = 'Highlight when yanking (copying) text',
+    callback = function()
+	vim.hl.on_yank()
+    end,
 })
 
 -- Sync clipboard between OS and Neovim. Schedule the setting after `UIEnter` because it can
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
 -- See `:h 'clipboard'`
 vim.api.nvim_create_autocmd('UIEnter', {
-	callback = function()
-		vim.o.clipboard = 'unnamedplus'
-	end,
+    callback = function()
+	vim.o.clipboard = 'unnamedplus'
+    end,
 })
 
 -- USER COMMANDS: DEFINE CUSTOM COMMANDS
@@ -81,31 +83,29 @@ vim.api.nvim_create_autocmd('UIEnter', {
 -- See `:h nvim_create_user_command()` and `:h user-commands`
 
 -- Create a command `:GitBlameLine` that print the git blame for the current line
-vim.api.nvim_create_user_command('GitBlameLine', function()
-	local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-	local filename = vim.api.nvim_buf_get_name(0)
-	print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
-end, { desc = 'Print the git blame for the current line' })
+--  vim.api.nvim_create_user_command('GitBlameLine', function()
+--      local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+--      local filename = vim.api.nvim_buf_get_name(0)
+--      print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
+--  end, { desc = 'Print the git blame for the current line' })
 
 -- PLUGINS
 
 vim.pack.add({
-	-- colorschemes
-	"https://github.com/folke/tokyonight.nvim",
-	"https://github.com/shaunsingh/nord.nvim",
-	"https://github.com/vague-theme/vague.nvim",
-	"https://github.com/kepano/flexoki-neovim",
-	"https://github.com/kdheepak/monochrome.nvim",
-	"https://github.com/shatur/neovim-ayu",
-	"https://github.com/ellisonleao/gruvbox.nvim",
-	"https://github.com/rebelot/kanagawa.nvim",
-
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/ibhagwan/fzf-lua",
-	-- "https://github.com/lewis6991/gitsigns.nvim", <-- git integration
+    "https://github.com/rebelot/kanagawa.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/ibhagwan/fzf-lua",
+    "https://github.com/lewis6991/gitsigns.nvim",
+    "https://github.com/tpope/vim-fugitive",
+    "https://github.com/saghen/blink.cmp",
+    "https://github.com/saghen/blink.lib",
 })
 
 require("fzf-lua")
+
+local cmp = require('blink.cmp')
+cmp.build():pwait()
+cmp.setup()
 
 -- fzf-lua keybindings
 vim.keymap.set({ "n" }, "<leader>f", function() FzfLua.files() end)
@@ -117,34 +117,30 @@ vim.cmd("colorscheme kanagawa")
 -- Enable LSPs
 
 vim.lsp.enable({
-	"ccls",
-	"zls",
-	"ts_ls",
-	"jsonls",
-	"html",
-	"pylsp",
+    "ccls",
+    "zls",
+    "ts_ls",
+    "jsonls",
+    "html",
+    "pylsp",
 })
-
-  --vim.diagnostic.config({
-  --	virtual_text = true
-  --})
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup("my.lsp", {}),
 	callback = function(env)
-		local client = assert(vim.lsp.get_client_by_id(env.data.client_id))
+	    local client = assert(vim.lsp.get_client_by_id(env.data.client_id))
 
-		vim.keymap.set({ "n" }, "<leader>d", function() vim.diagnostic.open_float() end)
+	    vim.keymap.set({ "n" }, "<leader>d", function() vim.diagnostic.open_float() end)
 
-		if client:supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				group = vim.api.nvim_create_augroup("my.lsp", {clear=false}),
-				buffer = env.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = env.buf, id = client.id, timeout_ms = 1000 })
-				end,
-			})
-		end
+	    if client:supports_method('textDocument/formatting') then
+		vim.api.nvim_create_autocmd('BufWritePre', {
+		    group = vim.api.nvim_create_augroup("my.lsp", {clear=false}),
+		    buffer = env.buf,
+		    callback = function()
+		    vim.lsp.buf.format({ bufnr = env.buf, id = client.id, timeout_ms = 1000 })
+		end,
+	    })
 	end
+    end
 })
 
